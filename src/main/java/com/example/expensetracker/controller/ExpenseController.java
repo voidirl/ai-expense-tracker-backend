ee1f95d57c8cf4cc26af14b8dfe07a6fa699a2d5
voidirl <rishavrajput204@gmail.com>
2026-03-20 12:27:28 +0530

    dockerize frontend and backend

--- src/main/java/com/example/expensetracker/controller/ExpenseController.java
+++ src/main/java/com/example/expensetracker/controller/ExpenseController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/expenses")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ExpenseController {
 
     private final ExpenseService expenseService;
